411d4eac44eb61463f8f9744772b4e705e928ba9
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-19 15:23:05 -0600

    allows a card to be randomly filled with a specified range of numbers and with an 'x' in the center, and also replaces a number with an 'x' when that number is called

--- bingo-kata/lib/bingo.rb
+++ bingo-kata/lib/bingo.rb
@@ -1,4 +1,6 @@
 class Card
+  attr_accessor :card
+
   def initialize(*card)
     @card = card
   end
@@ -11,5 +13,23 @@ class Card
       [],
       [],
     ]
+  end
+
+  def fill_card
+    self.card.each do |row|
+      row << rand(1..15)
+      row << rand(16..30)
+      row << rand(31..45)
+      row << rand(46..60)
+      row << rand(61..75)
+    end
+    card[3][2] = "x"
+  end
+  def mark_card(called_number)
+    self.card.each do |row|
+      if row.include?(called_number)
+        row.collect! {|number| (number == called_number) ? "x" : number }
+      end
+    end
   end
 end
--- bingo-kata/spec/bingo_spec.rb
+++ bingo-kata/spec/bingo_spec.rb
@@ -2,9 +2,10 @@ require 'rspec/core'
 require 'bingo.rb'
 
 describe Card do
+
+  before { @new_card = Card.new }
   it "should generate a card with 5 arrays" do
-    card = Card.new.create_card
-    expect(card).to eq [
+    expect(@new_card.create_card).to eq [
                           [],
                           [],
                           [],
@@ -12,4 +13,32 @@ describe Card do
                           [],
                        ]
   end
+  it "should generate each column with a number within the appropriate number range" do
+    @new_card.create_card
+    @new_card.fill_card
+    expect(@new_card.card[0][0].between?(1,15)).to eq true
+    expect(@new_card.card[1][0].between?(1,15)).to eq true
+    expect(@new_card.card[2][0].between?(1,15)).to eq true
+    expect(@new_card.card[3][0].between?(1,15)).to eq true
+    expect(@new_card.card[4][0].between?(1,15)).to eq true
+    expect(@new_card.card[0][1].between?(16,30)).to eq true
+    expect(@new_card.card[0][2].between?(31,45)).to eq true
+    expect(@new_card.card[0][3].between?(46,60)).to eq true
+    expect(@new_card.card[0][4].between?(61,75)).to eq true
+  end
+  it "automatically places an 'x' in the center of the card" do
+    @new_card.create_card
+    @new_card.fill_card
+    expect(@new_card.card[3][2]).to eq "x"
+  end
+
+  it "allows a user to mark their card with an x in replacement of a called number" do
+    @new_card.create_card
+    @new_card.fill_card
+    @new_card.card[0][0] = 1
+    @new_card.mark_card(1)
+    expect(@new_card.card[0][0]).to eq "x"
+  end
 end
+
+
